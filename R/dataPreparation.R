6897a14b519eb6f9d8e607a15c220bee46a022a5
gross47 <volker.vongross@uni-goettingen.de>
2020-11-09 12:14:53 +0100

    update dataPrep

--- R/dataPreparation.R
+++ R/dataPreparation.R
@@ -10,6 +10,7 @@
 #'
 #' @param dat Untransformed data table as shown in the example.
 #' @param uncertainty Indicates whether the uncertainty shall be repesented by SE or SD. Please be aware that the respective chosen uncertainty must be included in the data. Best would be to consider the format of the exemplary data (database.xlsx) in the GitLab.
+#' @param expVAL Indicates how the expected Value should be represented
 #' @return An initialized landUse object ready for initScenario.
 
 
@@ -18,31 +19,31 @@
 #' @export
 dataPreparation <- function(dat, uncertainty = "SE", expVAL = "mean"){   #added expected value
 
-    ## Convert input Data to dat.final ## 
+    ## Convert input Data to dat.final ##
     ## Filter all Rows with only NA ##
-    dat.final <- dat[rowSums(is.na(dat)) != ncol(dat), ] 
+    dat.final <- dat[rowSums(is.na(dat)) != ncol(dat), ]
     if(any(is.na(dat.final[, 1]))){dat.final <- dat.final[-1, ]}
-    
+
     ## Create column names ##
     colnames(dat.final) <- dat.final[1, ]
     dat.final <- dat.final[-1, ]
-    
+
     ## rename duplicated Columnnames ##
     names(dat.final) <- make.unique(colnames(dat.final))
-    
+
     ## detect and set classes of a dat.final
     dat.final <- lapply(dat.final, type.convert) %>% bind_cols()
-    
+
     ## rename first columns for initScenario function and define data structure ##
     chtr.cols <- unlist(lapply(dat.final[1,],is.numeric))
     chtr.cols <- length(chtr.cols[chtr.cols == FALSE])
     dat.final[, (chtr.cols+1):ncol(dat.final)][is.na(dat.final[, (chtr.cols+1):ncol(dat.final)])] <- 0
-    
+
     ## warn and delete factor rows with NA ##
     if(any(is.na(dat.final[, 1:chtr.cols]))){warning("Some Indicators have missing value, rows got deleted")}
     # which(is.na(dat.final[, 1:chtr.cols]), arr.ind = TRUE)
     dat.final <- na.omit(dat.final)
-    
+
     ## select landUse names ##
     landUse <- dat[1, ]
     landUse <- landUse[, colSums(is.na(landUse)) != nrow(landUse)]
