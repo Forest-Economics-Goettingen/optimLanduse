8a1da3c72cba8b1ea1bdc5a26c984de54fed6877
kaihusmann <khusman@gwdg.de>
2020-11-24 21:08:26 +0100

    corr defineConstraintCoefficients

--- R/helper.R
+++ R/helper.R
@@ -24,14 +24,10 @@ defineObjectiveCoefficients <- function(scenarioTable) {
 #' @export
 defineConstraintCoefficients <- function (scenarioTable) {
   tempTableMore <- scenarioTable %>% filter(direction == "more is better") %>%
-    mutate(across(starts_with("adjSem"),
-                  ~{(. - minAdjSem) / diffAdjSem},
-                  .names = "{.col}_modified"))
+    mutate_at(vars(starts_with("adjSem")), funs(modified = (. - minAdjSem) / diffAdjSem))
 
   tempTableLess <- scenarioTable %>% filter(direction == "less is better") %>%
-    mutate(across(starts_with("adjSem"),
-                  ~{(maxAdjSem - .) / diffAdjSem},
-                  .names = "{.col}_modified"))
+    mutate_at(vars(starts_with("adjSem")), funs(modified = (maxAdjSem - .) / diffAdjSem))
 
   tempTableMore %>% bind_rows(tempTableLess) %>% select(ends_with("modified")) %>% as.matrix() %>%
     return()
