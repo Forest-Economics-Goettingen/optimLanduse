8fa27ea97a1703c33680049bd7016d39892477d9
khusman1@gwdg.de <kai.husmann@uni-goettingen.de>
2020-12-03 09:35:17 +0100

    Minor changes to improve conformity to formal package requirements for CRAN

--- R/helper.R
+++ R/helper.R
@@ -3,6 +3,13 @@
 #----------------------------------------------------------#
 # Calculate coefficients, one for each variable from the scenario table
 
+#' Internal function for the re-formulation of the long represantation of the coefficients of the robust optimization into nLanduse Parameters. Advantage is that the shorter representation corresponds the expected form of the objective function of the lpSolveAPI package.
+#'
+#'
+#' @param scenarioTable The fully initialized scenario table.
+#' @return The coefficients of the objective function.
+
+
 #' @export
 defineObjectiveCoefficients <- function(scenarioTable) {
   # Set "less is better to negative" and divide by maximum difference
@@ -20,6 +27,13 @@ defineObjectiveCoefficients <- function(scenarioTable) {
 #-------------------------------------#
 #### Define the constraints matrix ####
 #-------------------------------------#
+
+#' Internal function for the re-formulation of the long represantation of the constraints of the robust optimization into nLanduse colums (but still the original row length). Advantage is that the representation corresponds the expected form of the constraints of the lpSolveAPI package.
+#'
+#'
+#' @param scenarioTable The fully initialized scenario table.
+#' @return The coefficients of the objective function.
+
 #' @export
 defineConstraintCoefficients <- function (scenarioTable) {
   tempTableMore <- scenarioTable %>% filter(direction == "more is better") %>%
